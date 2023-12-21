66fd9f430b59c7ba6f573a72cd52c8d017337977
lirena <sakshamkushwaha12776@gmail.com>
2023-12-21 20:03:03 +0530

    initial commit

---
+++ KronchHIProvider/build.gradle.kts
@@ -0,0 +1,26 @@
+// use an integer for version numbers
+version = 32
+
+
+cloudstream {
+    language = "hi"
+    // All of these properties are optional, you can safely remove them
+
+    description = "With this extension you will be able to watch hindi hardsubbed/dubbed anime available in the Indian catalog of Crunchyroll."
+    authors = listOf("Stormunblessed, KillerDogeEmpire, lirena")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Anime",
+        "OVA",
+    )
+
+    iconUrl = "https://raw.githubusercontent.com/Stormunblessed/IPTV-CR-NIC/main/logos/kronch.png"
+}
---
+++ KronchHIProvider/src/main/kotlin/com/lirena/KronchHIProviderPlugin.kt
@@ -0,0 +1,29 @@
+package com.KillerDogeEmpire
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import android.os.Handler
+
+@CloudstreamPlugin
+class KronchHIProviderPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(KronchHIProvider())
+    }
+
+    companion object {
+        /**
+         * Used to make Runnables work properly on Android 21
+         * Otherwise you get:
+         * ERROR:D8: Invoke-customs are only supported starting with Android O (--min-api 26)
+         **/
+        inline fun Handler.postFunction(crossinline function: () -> Unit) {
+            this.post(object : Runnable {
+                override fun run() {
+                    function()
+                }
+            })
+        }
+    }
+}
