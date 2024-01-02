045294ddcf676f0ee9ec0413659ad1ee7f35ac3a
Saksham Kushwaha <92094239+LiReNa00@users.noreply.github.com>
2024-01-02 13:51:50 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Stormunblessed/kamy-cs3")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/LiReNa00/CloudstreamKronchHIProvider/")
 
         authors = listOf("Stormunblessed")
     }
