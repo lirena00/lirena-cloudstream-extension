0e32915a2cb391ec9197f1ca442097d98faf1ca4
lirena <sakshamkushwaha12776@gmail.com>
2024-01-03 16:41:45 +0530

    .

--- AnimesagaProvider/src/main/kotlin/com/example/AnimsagaProviderPlugin.kt
+++ AnimesagaProvider/src/main/kotlin/com/example/AnimsagaProviderPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class AnimsagaProivderPlugin: Plugin() {
+class AnimesagaProivderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(AnimesagaProvider())
